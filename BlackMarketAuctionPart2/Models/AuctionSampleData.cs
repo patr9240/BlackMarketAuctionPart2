a7977985def1be5cdc38525122cdcb2a0546a8a1
patr9240 <patr9240@gmail.com>
2016-08-09 15:39:19 -0400

    Added images for all current auction items

--- BlackMarketAuctionPart2/Models/AuctionSampleData.cs
+++ BlackMarketAuctionPart2/Models/AuctionSampleData.cs
@@ -33,25 +33,25 @@ namespace BlackMarketAuctionPart2.Models
             new List<Item>
             {
                 //Mounts
-                new Item { Name = "Zulian Tiger", Price = 10.0M, Description = "Ferocious tiger kept by the High Priest Thekal.", ItemImage = "/Assets/Images/Auction/Placeholder.gif", Category = Categories.Single(c => c.Name == "Mounts"), },
-                new Item { Name = "Invincible", Price = 100.0M, Description = "The mighty steed of Arthas Menethil.", ItemImage = "/Assets/Images/Auction/Placeholder.gif", Category = Categories.Single(c => c.Name == "Mounts"), },
+                new Item { Name = "Zulian Tiger", Price = 10.0M, Description = "Ferocious tiger kept by the High Priest Thekal.", ItemImage = "/Assets/Images/Auction/Items/ZulianTiger.jpg", Category = Categories.Single(c => c.Name == "Mounts"), },
+                new Item { Name = "Invincible", Price = 100.0M, Description = "The mighty steed of Arthas Menethil.", ItemImage = "/Assets/Images/Auction/Items/Invincible.jpg", Category = Categories.Single(c => c.Name == "Mounts"), },
                 //Pets
-                new Item { Name = "Spectal Tiger Cub", Price = 10.0M, Description = "Affectionate and curious spectral kitten.", ItemImage = "/Assets/Images/Auction/Placeholder.gif", Category = Categories.Single(c => c.Name == "Pets"), },
-                new Item { Name = "Hyppogryph Hatchling", Price = 10.0M, Description = "Orphaned Hyppogryph cared for by the Druids.", ItemImage = "/Assets/Images/Auction/Placeholder.gif", Category = Categories.Single(c => c.Name == "Pets"), },
+                new Item { Name = "Spectal Tiger Cub", Price = 10.0M, Description = "Affectionate and curious spectral kitten.", ItemImage = "/Assets/Images/Auction/Items/TigerCub.jpg", Category = Categories.Single(c => c.Name == "Pets"), },
+                new Item { Name = "Hippogryph Hatchling", Price = 10.0M, Description = "Orphaned Hippogryph cared for by the Druids.", ItemImage = "/Assets/Images/Auction/Items/HippogryphHatchling.jpg", Category = Categories.Single(c => c.Name == "Pets"), },
                 //Armour
-                new Item { Name = "FrostFire Regalia", Price = 10.0M, Description = "Frosty armour to keep you safe.", ItemImage = "/Assets/Images/Auction/Placeholder.gif", Category = Categories.Single(c => c.Name == "Armour"), },
-                new Item { Name = "Redemption Armour", Price = 10.0M, Description = "Redeem yourself with this protective plate armour.", ItemImage = "/Assets/Images/Auction/Placeholder.gif", Category = Categories.Single(c => c.Name == "Armour"), },
+                new Item { Name = "FrostFire Regalia", Price = 10.0M, Description = "Frosty armour to keep you safe.", ItemImage = "/Assets/Images/Auction/Items/FrostFireRegalia.jpg", Category = Categories.Single(c => c.Name == "Armour"), },
+                new Item { Name = "Redemption Armour", Price = 10.0M, Description = "Redeem yourself with this protective plate armour.", ItemImage = "/Assets/Images/Auction/Items/RedemptionArmour.jpg", Category = Categories.Single(c => c.Name == "Armour"), },
                 //Weapons
-                new Item { Name = "Sun-Lute of the Phoenix King", Price = 10.0M, Description = "Bust some riffs, or slay all that stand in your path.", ItemImage = "/Assets/Images/Auction/Placeholder.gif", Category = Categories.Single(c => c.Name == "Weapons"), },
-                new Item { Name = "Arcanite Ripper", Price = 10.0M, Description = "Bust some riffs, or slay all that stand in your path.", ItemImage = "/Assets/Images/Auction/Placeholder.gif", Category = Categories.Single(c => c.Name == "Weapons"), },
+                new Item { Name = "Sun-Lute of the Phoenix King", Price = 10.0M, Description = "Bust some riffs, or slay all that stand in your path.", ItemImage = "/Assets/Images/Auction/Items/SunLute.jpg", Category = Categories.Single(c => c.Name == "Weapons"), },
+                new Item { Name = "Arcanite Ripper", Price = 10.0M, Description = "Bust some riffs, or slay all that stand in your path.", ItemImage = "/Assets/Images/Auction/Items/ArcaniteRipper.jpg", Category = Categories.Single(c => c.Name == "Weapons"), },
                 //Toys
-                new Item { Name = "Foam Sword Rack", Price = 10.0M, Description = "Foam Sword Fight!", ItemImage = "/Assets/Images/Auction/Placeholder.gif", Category = Categories.Single(c => c.Name == "Toys"), },
-                new Item { Name = "Imp in a Ball", Price = 10.0M, Description = "Ask the Imp a question, I'm sure you'll like his answer.", ItemImage = "/Assets/Images/Auction/Placeholder.gif", Category = Categories.Single(c => c.Name == "Toys"), },
+                new Item { Name = "Foam Sword Rack", Price = 10.0M, Description = "Foam Sword Fight!", ItemImage = "/Assets/Images/Auction/Items/FoamSwordRack.jpg", Category = Categories.Single(c => c.Name == "Toys"), },
+                new Item { Name = "Imp in a Ball", Price = 10.0M, Description = "Ask the Imp a question, I'm sure you'll like his answer.", ItemImage = "/Assets/Images/Auction/Items/ImpInABall.jpg", Category = Categories.Single(c => c.Name == "Toys"), },
                 //Tabards
-                new Item { Name = "Tabard of Brilliance", Price = 10.0M, Description = "Show everyone how brilliant you are!", ItemImage = "/Assets/Images/Auction/Placeholder.gif", Category = Categories.Single(c => c.Name == "Tabards"), },
-                new Item { Name = "Tabard of Frost", Price = 10.0M, Description = "Show everyone how frosty you are!", ItemImage = "/Assets/Images/Auction/Placeholder.gif", Category = Categories.Single(c => c.Name == "Tabards"), },
+                new Item { Name = "Tabard of Brilliance", Price = 10.0M, Description = "Show everyone how brilliant you are!", ItemImage = "/Assets/Images/Auction/Items/TabardBrilliance.jpg", Category = Categories.Single(c => c.Name == "Tabards"), },
+                new Item { Name = "Tabard of Frost", Price = 10.0M, Description = "Show everyone how frosty you are!", ItemImage = "/Assets/Images/Auction/Items/TabardFrost.jpg", Category = Categories.Single(c => c.Name == "Tabards"), },
                 //Mystery
-                new Item { Name = "Mysterious Black Market Container", Price = 10.0M, Description = "A chest filled with goodies... Or not.", ItemImage = "/Assets/Images/Auction/Placeholder.gif", Category = Categories.Single(c => c.Name == "Mystery"), }
+                new Item { Name = "Mysterious Black Market Container", Price = 10.0M, Description = "A chest filled with goodies... Or not.", ItemImage = "/Assets/Images/Auction/Items/UnclaimedContainer.jpg", Category = Categories.Single(c => c.Name == "Mystery"), }
 
             }.ForEach(i => context.Items.Add(i));
         }
